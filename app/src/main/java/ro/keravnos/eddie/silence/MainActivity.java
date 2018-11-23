4a7ac8d15eb1de5d18c4252787790c362c10c7fe
Gavrilovici Eduard <33176335+GavriloviciEduard@users.noreply.github.com>
2018-11-24 00:47:59 +0200

    ui tweaks

--- app/src/main/java/ro/keravnos/eddie/silence/MainActivity.java
+++ app/src/main/java/ro/keravnos/eddie/silence/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
 
-    //private TextView mTextMessage;
+    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,12 +20,15 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected( @NonNull MenuItem item ) {
             switch (item.getItemId()) {
                 case R.id.locations:
+                    mTextMessage.setText("CEVA1");
 
                     return true;
                 case R.id.map:
+                    mTextMessage.setText("CEVA2");
 
                     return true;
                 case R.id.settings:
+                    mTextMessage.setText("CEVA3");
 
                     return true;
             }
@@ -36,10 +40,17 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate( Bundle savedInstanceState )
     {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextMessage =  findViewById(R.id.message);
+
+
         BottomNavigationView navigation =  findViewById(R.id.navigation);
+        Integer indexItem = 1;
+        navigation.getMenu().getItem(indexItem).setChecked(true);
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
