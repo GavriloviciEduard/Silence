b25e9044a0c1cdfaae0472619a0cae0ac9163c74
Gabi1M <ig_999@yahoo.com>
2018-11-26 18:03:35 +0200

    Camera focuses on location

--- app/src/main/java/ro/keravnos/eddie/silence/Fragment/MapFragment.java
+++ app/src/main/java/ro/keravnos/eddie/silence/Fragment/MapFragment.java
@@ -2,18 +2,14 @@ package ro.keravnos.eddie.silence.Fragment;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.app.NotificationManager;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 
@@ -22,9 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import  ro.keravnos.eddie.silence.MainActivity;
-
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -32,11 +25,13 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import ro.keravnos.eddie.silence.R;
 
-public class MapFragment extends Fragment {
+import static android.support.v4.content.ContextCompat.getSystemService;
+
+public class MapFragment extends Fragment
+{
     public MapView mMapView;
     public GoogleMap googleMap;
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
@@ -57,69 +52,66 @@ public class MapFragment extends Fragment {
 
                 googleMap = mMap;
 
-
                 googleMap.setMyLocationEnabled(true);
 
-                LatLng Sydney = new LatLng(-34,151);
 
-                googleMap.addMarker(new MarkerOptions().position(Sydney).title("Marker Sydney").snippet("Marker Description"));
+                Criteria criteria = new Criteria();
+                LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                String provider = locationManager.getBestProvider(criteria, true);
+                Location location = locationManager.getLastKnownLocation(provider);
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng myPosition = new LatLng(latitude, longitude);
 
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(Sydney).zoom(12).build();
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(myPosition).zoom(20).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
     }
 
     @Override
-    public View onCreateView( @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
-
-
+    public View onCreateView( @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
+    {
 
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
         mMapView = rootView.findViewById(R.id.mapView);
+
         mMapView.onCreate(savedInstanceState);
 
         mMapView.onResume();
 
-        try {
+        try
+        {
             MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
+        }
+
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
-        //if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED )
-        //{
-        //ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
-        //}
-
-        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Should we show an explanation?
-
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
             if (shouldShowRequestPermissionRationale( android.Manifest.permission.ACCESS_FINE_LOCATION))
             {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
             }
 
             else
             {
-                // No explanation needed, we can request the permission.
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
 
-
         else
+            {
             set_map();
-
+        }
         return rootView;
     }
+
 
     @Override
     public void onResume()
@@ -152,17 +144,14 @@ public class MapFragment extends Fragment {
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
+                                           String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
             case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION :
                 {
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
                     Toast.makeText(getActivity().getApplicationContext(), " Location Permission granted", Toast.LENGTH_SHORT).show();
                     set_map();
 
@@ -170,16 +159,10 @@ public class MapFragment extends Fragment {
 
                 else
                 {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                     Toast.makeText(getActivity().getApplicationContext(), "Location Permission denied", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 }
--- app/src/main/java/ro/keravnos/eddie/silence/MainActivity.java
+++ app/src/main/java/ro/keravnos/eddie/silence/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +15,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import ro.keravnos.eddie.silence.Model.BottomNav;
 import ro.keravnos.eddie.silence.Model.ViewPagerAdapter;
